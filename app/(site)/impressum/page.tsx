84d1e49f2c3cf552e879a9e2049e176fe16d8078
ranaaksoy <aksoy.rana@chadev.ch>
2024-07-25 11:13:35 +0200

    .env updated

--- app/(site)/impressum/page.tsx
+++ app/(site)/impressum/page.tsx
@@ -3,8 +3,8 @@ import Impressum from "../../../components/Impressum";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
-  title: "About",
-  description: "About page",
+  title: "",
+  description: "",
 };
 
 export default function UeberUnsPage() {
